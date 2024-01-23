ba01133ee334f6b8887a18b1dd9c508eb104a1e4
Hyegyeong <gju06061@naver.com>
2024-01-23 17:38:52 +0900

    [FMA-64] chore: add ktlint plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
-
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 true // Needed to make the Suppress annotation work for the plugins block
