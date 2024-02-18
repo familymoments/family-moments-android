25f31350edd3f26963721ebcef64f2e24117f2c6
hong <sdhong0609@gmail.com>
2024-02-18 20:56:18 +0900

    chore: change BottomNavRoute to enum class

--- app/src/main/java/io/familymoments/app/feature/bottomnav/model/BottomNavItem.kt
+++ app/src/main/java/io/familymoments/app/feature/bottomnav/model/BottomNavItem.kt
@@ -10,41 +10,22 @@ sealed class BottomNavItem(
     @StringRes val labelResId: Int
 ) {
     data object Home :
-        BottomNavItem(BottomNavDestination.Home.route, R.drawable.ic_bottom_nav_home, R.string.bottom_nav_label_home)
+        BottomNavItem(BottomNavRoute.HOME.name, R.drawable.ic_bottom_nav_home, R.string.bottom_nav_label_home)
 
     data object Album :
-        BottomNavItem(BottomNavDestination.Album.route, R.drawable.ic_bottom_nav_album, R.string.bottom_nav_label_album)
+        BottomNavItem(BottomNavRoute.ALBUM.name, R.drawable.ic_bottom_nav_album, R.string.bottom_nav_label_album)
 
     data object AddPost :
-        BottomNavItem(BottomNavDestination.AddPost.route, R.drawable.ic_bottom_nav_add_post, R.string.bottom_nav_label_home)
+        BottomNavItem(BottomNavRoute.ADD_POST.name, R.drawable.ic_bottom_nav_add_post, R.string.bottom_nav_label_home)
 
     data object Calendar :
-        BottomNavItem(BottomNavDestination.Calendar.route, R.drawable.ic_bottom_nav_calendar, R.string.bottom_nav_label_calendar)
+        BottomNavItem(BottomNavRoute.CALENDAR.name, R.drawable.ic_bottom_nav_calendar, R.string.bottom_nav_label_calendar)
 
     data object MyPage :
-        BottomNavItem(BottomNavDestination.MyPage.route, R.drawable.ic_bottom_nav_my_page, R.string.bottom_nav_label_my_page)
+        BottomNavItem(BottomNavRoute.MY_PAGE.name, R.drawable.ic_bottom_nav_my_page, R.string.bottom_nav_label_my_page)
 }
 
-sealed interface BottomNavDestination {
-    val route: String
 
-    data object Home : BottomNavDestination {
-        override val route: String = "Home"
-    }
-
-    data object Album : BottomNavDestination {
-        override val route: String = "Album"
-    }
-
-    data object AddPost : BottomNavDestination {
-        override val route: String = "AddPost"
-    }
-
-    data object Calendar : BottomNavDestination {
-        override val route: String = "Calendar"
-    }
-
-    data object MyPage : BottomNavDestination {
-        override val route: String = "MyPage"
-    }
+enum class BottomNavRoute {
+    HOME, ALBUM, ADD_POST, CALENDAR, MY_PAGE
 }
