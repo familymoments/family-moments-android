7ac40c35c7e0d8a8e50b0515f7722a9aa5c8d606
boring-km <kms0644804@naver.com>
2024-06-08 21:31:30 +0900

    fix: 소셜 회원가입 후 성공 시 메인 화면으로 이동하기

--- app/src/main/java/io/familymoments/app/feature/signup/activity/SocialSignUpActivity.kt
+++ app/src/main/java/io/familymoments/app/feature/signup/activity/SocialSignUpActivity.kt
@@ -1,9 +1,11 @@
 package io.familymoments.app.feature.signup.activity
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
 import io.familymoments.app.core.base.BaseActivity
 import io.familymoments.app.core.network.dto.response.LoginResult
+import io.familymoments.app.feature.bottomnav.activity.MainActivity
 import io.familymoments.app.feature.signup.screen.SocialSignUpScreen
 import io.familymoments.app.feature.signup.viewmodel.SignUpViewModel
 
@@ -26,7 +28,10 @@ class SocialSignUpActivity : BaseActivity<SignUpViewModel>(SignUpViewModel::clas
             socialToken,
             socialLoginResult
         ) {
-
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
         }
     }
 }
